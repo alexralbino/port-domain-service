081bef614971e40a3838b40d519be4e3841176ff
Alexandre Rocha <alexandreflash1996@hotmail.com>
2023-11-27 13:01:28 -0300

    Adding gracefull shutdown

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// Handle signals for graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	
+	go func() {
+		sig := <-stop
+		log.Printf("Received signal %v. Shutting down gracefully...", sig)
+		// Perform any cleanup or additional shutdown logic here
+		os.Exit(0)
+	}()
+
+	log.Println("Port Service is running. Press Ctrl+C to stop.")
+	select {}
+}
